eab501f03c3b8bf81f568c407f4ff0f4d9e83c75
HojinAn <asdv94@naver.com>
2021-10-26 15:54:55 +0900

    2.2 2D context

--- README.md
+++ README.md
@@ -3,8 +3,15 @@
 > ### HTML
 >
 > CSS를 작업할 때는 class를 사용, JavaScript를 작업할 때는 ID 사용
+> canvas는 context를 갖는다는 특징이 있다.
+>
+> > #### context
+> >
+> > canvas 안에서 픽셀에 접근할 수 있는 방법이다.
+> > JS에서 ctx등의 contesxt variable을 만들고 접근하면 된다.
 
 > ### JS
 >
 > mousemove의 이벤트에서 clientX,Y는 윈도우 전체 범위 내에서 마우스 위치값을 나타낸다.
-> offsetX,Y는 우리가 이용하고자 하는 태그? canvas 안에서의 위치를 나타낸다. 이벤트 안에서는 offset 사용 
+> offsetX,Y는 우리가 이용하고자 하는 태그? canvas 안에서의 위치를 나타낸다. 이벤트 안에서는 offset 사용
+> canvas에게 pixel이 다뤄질 크기를 알려줘야지 ctx가 동작한다.
--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+//canvas를 element로 만드는 거기 때문에 width, height를 지정해줘야 한다.
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,24 +14,27 @@ function stopPainting() {
   painting = false;
 }
 
+function startPainting() {
+  painting = true;
+}
 function onMouseMove(event) {
   // console.log(event); 개발자도구 console에서 확인 해보기
   const x = event.offsetX;
   const y = event.offsetY;
   // console.log(x, y); 역시 console로 제대로 작동되는지 확인해보기
-}
-
-function onMouseDown(event) {
-  painting = true;
-}
-
-function onMouseUp(event) {
-  stopPainting();
+  if (!painting) {
+    ctx.beginPath(); //mouse가 들어설 때부터 path 시작
+    ctx.moveTo(x, y); // path를 만든다
+    // 아마 클릭할 때 마우스 위치를 알기 위해서 ctx의 위치를 계속 tracking해주는 것 같다.
+  } else {
+    ctx.lineTo(x, y); //클릭하고 있지 않을 때 만들어진 path로부터 line 만들어지기 시작
+    ctx.stroke(); //순간순간 계속 작은 라인을 만드는 것
+  } // 이 if else는 마우스가 canvas에서 움직이는 내내 실행된다. start point, ending point가 있는게 아니다.
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
